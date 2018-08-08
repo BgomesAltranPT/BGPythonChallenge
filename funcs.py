82c1632cab6b053d610f72256447a7154177f254
bgomesAltranPT <bgomesAltranPT@users.noreply.github.com>
2018-08-08 11:29:08 +0100

    v1.0.2

--- funcs.py
+++ funcs.py
@@ -5,9 +5,9 @@ import handler_json as jhandler
 import data
 
 
-
 def generate_xml():
     xhandler.xml_handler(data.data).generate()
+
 
 def generate_json():
     jhandler.json_handler().generate()
